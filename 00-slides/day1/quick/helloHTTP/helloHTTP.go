174c1b5585a9b89d3767138921f149a27e72fb09
Cory LaNou <cory@lanou.com>
2015-10-02 07:55:58 -0500

    refactor based on issue #3

--- 00-slides/day1/quick/helloHTTP/helloHTTP.go
+++ 00-slides/day1/quick/helloHTTP/helloHTTP.go
@@ -9,39 +9,83 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"net"
 	"net/http"
+	"strings"
 )
 
-type (
-	// hello holds a message.
-	hello struct {
-		Message string
-	}
-)
+// Response is used to send data back to the client
+type Response struct {
+	// Greeting contains the greeting in the proper language
+	Greeting string
+}
 
 // main is the entry point for the application.
 func main() {
 	http.HandleFunc("/english", helloEnglish)
 	http.HandleFunc("/chinese", helloChinese)
-	http.ListenAndServe("localhost:9999", nil)
+
+	addr := "localhost:9999"
+	log.Printf("starting server on http://%s", addr)
+
+	log.Fatal(http.ListenAndServe(addr, nil))
 }
 
 // helloEnglish sends a greeting in English.
 func helloEnglish(w http.ResponseWriter, r *http.Request) {
-	err := json.NewEncoder(w).Encode(hello{"Hello World"})
+	err := json.NewEncoder(w).Encode(Response{Greeting: "Hello World"})
 	if err != nil {
-		log.Println("Error encoding JSON", err)
+		http.Error(w, "error encoding JSON", http.StatusInternalServerError)
 		return
 	}
-	log.Println("Sent English")
+	simpleLog(w, r)
 }
 
 // helloChinese sends a greeting in Chinese.
 func helloChinese(w http.ResponseWriter, r *http.Request) {
-	err := json.NewEncoder(w).Encode(hello{"你好世界"})
+	err := json.NewEncoder(w).Encode(Response{Greeting: "你好世界"})
 	if err != nil {
-		log.Println("Error encoding JSON", err)
+		http.Error(w, "error encoding JSON", http.StatusInternalServerError)
 		return
 	}
-	log.Println("Sent Chinese")
+	simpleLog(w, r)
+}
+
+// simpleLog is will log a very simple set of information for a request
+func simpleLog(w http.ResponseWriter, r *http.Request) {
+	// try to parse the remote address
+	host, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		host = r.RemoteAddr
+	}
+
+	uri := r.URL.RequestURI()
+
+	referer := r.Referer()
+	if referer == "" {
+		referer = "-"
+	}
+
+	userAgent := r.UserAgent()
+	if userAgent == "" {
+		userAgent = "-"
+	}
+
+	username := "-"
+	// Try to get it from the authorization header if set there
+	if u, _, ok := r.BasicAuth(); ok {
+		username = u
+	}
+
+	fields := []string{
+		host,
+		username,
+		r.Method,
+		uri,
+		r.Proto,
+		referer,
+		userAgent,
+	}
+
+	log.Println(strings.Join(fields, " "))
 }
